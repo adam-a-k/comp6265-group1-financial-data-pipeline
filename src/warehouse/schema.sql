1321bd7079d4d33be26b81ec7cd9b0191b30d50c
Adam Ahmed-Keyte <78147202+adam-a-k@users.noreply.github.com>
2026-04-11 09:37:31 +0100

    Updated schema according to data fetcher

--- src/warehouse/schema.sql
+++ src/warehouse/schema.sql
@@ -1,13 +1,37 @@
+DROP TABLE IF EXISTS stock_prices;
 CREATE TABLE IF NOT EXISTS stock_prices (
     timestamp TIMESTAMP,
+    open FLOAT,
+    high FLOAT,
+    low FLOAT,
+    close FLOAT,
+    volume FLOAT,
     symbol VARCHAR(10),
-    price_usd FLOAT
+    source VARCHAR(10)
 );
 
+DROP TABLE IF EXISTS forex_rates;
 CREATE TABLE IF NOT EXISTS forex_rates (
     timestamp TIMESTAMP,
-    pair VARCHAR(10),
-    rate FLOAT
+    open FLOAT,
+    high FLOAT,
+    low FLOAT,
+    close FLOAT,
+    volume FLOAT,
+    symbol VARCHAR(10),
+    source VARCHAR(10)
+);
+
+DROP TABLE IF EXISTS crypto_rates;
+CREATE TABLE IF NOT EXISTS crypto_rates (
+    timestamp TIMESTAMP,
+    open FLOAT,
+    high FLOAT,
+    low FLOAT,
+    close FLOAT,
+    volume FLOAT,
+    symbol VARCHAR(10),
+    source VARCHAR(10)
 );
 
 CREATE TABLE IF NOT EXISTS news_sentiment (
