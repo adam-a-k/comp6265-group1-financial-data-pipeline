4fccea83d9dac3f660a47c7432a4937fb8608e0c
Adam Ahmed-Keyte <78147202+adam-a-k@users.noreply.github.com>
2026-05-08 13:31:52 +0100

    Refactor ingestion and loading processes; add logging functionality

--- src/orchestration/scheduler.py
+++ src/orchestration/scheduler.py
@@ -9,23 +9,26 @@ from src.storage.lake_writer import write_to_lake
 def ingest():
     ts = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%S")
     for symbol in ["AAPL", "GOOGL", "MSFT"]:
-        df = StockFetcher().fetch(symbol)
-        write_to_lake(df, f"stocks/{symbol}/{ts}.csv")
+        stocks_df = StockFetcher().fetch(symbol)
+        write_to_lake(stocks_df, f"stocks/{symbol}/{ts}.csv")
 
     for from_sym, to_sym in [("USD", "EUR"), ("USD", "GBP")]:
-        df = ForexFetcher().fetch(from_symbol=from_sym, to_symbol=to_sym)
-        write_to_lake(df, f"forex/{from_sym}{to_sym}/{ts}.csv")
+        forex_df = ForexFetcher().fetch(from_symbol=from_sym, to_symbol=to_sym)
+        write_to_lake(forex_df, f"forex/{from_sym}{to_sym}/{ts}.csv")
 
     for coin in ["bitcoin", "ethereum"]:
-        df = CryptoFetcher().fetch(coin, vs_currency="usd", days=30)
-        write_to_lake(df, f"crypto/{coin}/{ts}.csv")
+        crypto_df = CryptoFetcher().fetch(coin, vs_currency="usd", days=30)
+        write_to_lake(crypto_df, f"crypto/{coin}/{ts}.csv")
 
-def etl():
+    return stocks_df, forex_df, crypto_df
 
-    load_to_db()
+def etl(df):
+
+    load_to_db(df)
 
 
-ingest()
+stocks_df, forex_df, crypto_df = ingest()
+#etl(stocks_df)
 
 # schedule.every(5).minutes.do(ingest)
 # schedule.every().hour.do(etl)
--- src/warehouse/load.py
+++ src/warehouse/load.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from src.warehouse.db import engine
-from src.etl.transform import transform
+from src.etl.transform import transform_stocks, transform_crypto, transform_forex
 
 
-def load_to_db():
+def load_to_db(df):
 
-    df = transform()
+    df = transform_stocks(df)
 
     df.to_sql(
         "stock_prices",
