796f02b9138d15ba2d8845601862b9f9f6a56ec3
Adam Ahmed-Keyte <78147202+adam-a-k@users.noreply.github.com>
2026-05-10 12:45:03 +0100

    Correcting flask dist to reflect folder structure change

--- src/dashboard/backend/app.py
+++ src/dashboard/backend/app.py
@@ -13,7 +13,7 @@ DB_URI = (
 engine = create_engine(DB_URI)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DIST_DIR = os.path.join(BASE_DIR, '..', 'dist')
+DIST_DIR = os.path.join(BASE_DIR, '..', 'frontend', 'dist')
 
 app = Flask(__name__, static_folder=DIST_DIR, static_url_path='')
 #app = Flask(__name__, static_folder=os.path.join('..', 'dist'), static_url_path='')
